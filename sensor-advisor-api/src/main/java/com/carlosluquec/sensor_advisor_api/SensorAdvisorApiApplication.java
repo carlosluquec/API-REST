f5f2b9c47136606fd9a46a66abbcb1e5bacfffc3
carlosluquec <carlosluq1@hotmail.com>
2025-11-16 19:39:41 -0500

    chrore: spring initializr project structure with dependencies

---
+++ sensor-advisor-api/src/main/java/com/carlosluquec/sensor_advisor_api/SensorAdvisorApiApplication.java
@@ -0,0 +1,13 @@
+package com.carlosluquec.sensor_advisor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorAdvisorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorAdvisorApiApplication.class, args);
+	}
+
+}
---
+++ sensor-advisor-api/src/test/java/com/carlosluquec/sensor_advisor_api/SensorAdvisorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosluquec.sensor_advisor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorAdvisorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
